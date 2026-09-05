e330db1df1ae2942f105b81038af1a3c65584b42
Sania-Raza <sania.raza.pk@gmail.com>
2026-09-05 23:25:29 +0500

    Change multer storage to memory storage
    Refactor multer configuration to use memory storage instead of disk storage, removing file system checks and upload directory creation.

--- backend/multer.js
+++ backend/multer.js
@@ -1,25 +1,5 @@
 const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
 
-const uploadDir = path.join(__dirname, "uploads");
-
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir);
-  },
-
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-
-    const ext = path.extname(file.originalname);
-
-    cb(null, path.basename(file.originalname, ext) + "-" + uniqueSuffix + ext);
-  },
-});
+const storage = multer.memoryStorage();
 
 exports.upload = multer({ storage });
