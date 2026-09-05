a12ce542aef8ef4711c806d076eb087fa62ae66c
Sania-Raza <sania.raza.pk@gmail.com>
2026-09-05 20:45:21 +0500

    Implement loading state in FeaturedProduct component
    Added loading state handling to display a loader while products are being fetched.

--- frontend/src/components/Route/FeaturedProduct/FeaturedProduct.jsx
+++ frontend/src/components/Route/FeaturedProduct/FeaturedProduct.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import styles from "../../../styles/styles";
 import ProductCard from "../ProductCard/ProductCard";
+import Loader from "../../Layout/Loader";
 
 const FeaturedProduct = () => {
-  const { allProducts } = useSelector((state) => state.products);
+  const { allProducts, isLoading } = useSelector(
+    (state) => state.products
+  );
 
   return (
     <div>
@@ -12,16 +15,18 @@ const FeaturedProduct = () => {
         <div className={`${styles.heading}`}>
           <h1>Featured Products</h1>
         </div>
-        <div className="grid grid-cols-2 gap-3 md:grid-cols-2 sm:gap-5 md:gap-6.25 lg:grid-cols-4 lg:gap-6.25 xl:grid-cols-5 xl:gap-7.5 mb-12 border-0">
-          {allProducts && allProducts.length !== 0 && (
-            <>
-              {allProducts &&
-                allProducts.map((i, index) => (
-                  <ProductCard data={i} key={index} />
-                ))}
-            </>
-          )}
-        </div>
+
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className="grid grid-cols-2 gap-3 md:grid-cols-2 sm:gap-5 md:gap-6.25 lg:grid-cols-4 lg:gap-6.25 xl:grid-cols-5 xl:gap-7.5 mb-12 border-0">
+            {allProducts &&
+              allProducts.length !== 0 &&
+              allProducts.map((i, index) => (
+                <ProductCard data={i} key={index} />
+              ))}
+          </div>
+        )}
       </div>
     </div>
   );
